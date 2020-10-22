42d2289d96ef19c044282f99b3a8610d5a2da689
miguelptome <miguelptome@gmail.com>
2020-10-22 21:29:28 +0100

    Add an implementation of a Full Adder 3 bit VHDL project that uses the full 1 bit adder.

---
+++ Library/Adder3Bit/Adder3Bit_Proj/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -o "C:/project/GIT/vhdl-projects/Library/Adder3Bit/Adder3Bit_Proj/adder3bit_isim_beh.exe" -prj "C:/project/GIT/vhdl-projects/Library/Adder3Bit/Adder3Bit_Proj/adder3bit_beh.prj" "work.adder3bit" 
