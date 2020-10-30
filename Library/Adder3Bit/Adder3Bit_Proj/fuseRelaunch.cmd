f01b225c68f03bd36c71086ab5de1caeeca6142a
miguelptome <miguelptome@gmail.com>
2020-10-30 14:59:19 +0000

    Refactor adderNbit

--- Library/Adder3Bit/Adder3Bit_Proj/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -o "C:/project/GIT/vhdl-projects/Library/Adder3Bit/Adder3Bit_Proj/adder3bit_isim_beh.exe" -prj "C:/project/GIT/vhdl-projects/Library/Adder3Bit/Adder3Bit_Proj/adder3bit_beh.prj" "work.adder3bit" 
