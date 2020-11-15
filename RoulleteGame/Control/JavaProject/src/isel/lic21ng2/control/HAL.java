eee8f2d89273b9acdc1e11dcd2d6b3e8e67e3d8d
miguelptome <miguelptome@gmail.com>
2020-11-15 21:34:51 +0000

    Add the base to the Roulette "Control" java project.

---
+++ RoulleteGame/Control/JavaProject/src/isel/lic21ng2/control/HAL.java
@@ -0,0 +1,83 @@
+package isel.lic21ng2.control;
+
+/**
+ * This class have has the main purpose to visualize 
+ * the access to the system UsbPort.
+ * 
+ * @since 11/15/2020
+ * 
+ * @author Miguel Tome'
+ */
+public class HAL {
+
+	/**
+	 * This method starts the class.
+	 * 
+	 * @param void
+	 * @return void
+	 */
+	public static void init() {
+		
+		// TODO check what is the purpose of this method.
+	}
+	
+	/**
+	 * This method return true if the bit have the value '1' and
+	 * false otherwise.
+	 * 
+	 * @param mask
+	 * @return
+	 */
+	public static boolean isBit(int mask) {
+		
+		// TODO check what is the purpose of this method.
+		return false;
+	}
+	
+	/**
+	 * Return the value of the bits represented by the @param mask
+	 * presents at UsbPort.
+	 * 
+	 * @param mask
+	 * @return
+	 */
+	public static int readBits(int mask) {
+		
+		// TODO check what is the purpose of this method.
+		return -1;
+	}
+	
+	/**
+	 * Write on the bits represented by the @param mask, the value
+	 * of the @param value.
+	 * 
+	 * @param mask
+	 * @param value
+	 */
+	public static void writeBits(int mask, int value) {
+		
+		// TODO check what is the purpose of this method.
+	}
+	
+	/**
+	 * Set the bits represented by the @param mask, with the 
+	 * value '1'.
+	 * 
+	 * @param mask
+	 */
+	public static void setBits(int mask) {
+		
+		// TODO check what is the purpose of this method.
+	}
+	
+	/**
+	 * Set the bits represented by the @param mask, with the 
+	 * value '0'.
+	 * 
+	 * @param mask
+	 */
+	public static void clrBits(int mask) {
+		
+		// TODO check what is the purpose of this method.
+	}
+}
---
+++ RoulleteGame/Control/JavaProject/src/isel/lic21ng2/control/KBD.java
@@ -0,0 +1,50 @@
+package isel.lic21ng2.control;
+
+/**
+ * This class have the purpose of allowing read keys from 
+ * the keyboard. 
+ * Read the keys {['0'..'9'], '*', '#'} or none.
+ * 
+ * @since 11/15/2020
+ * 
+ * @author Miguel Tome'
+ */
+public class KBD {
+
+	public static final char NONE = 0;
+	
+	/**
+	 * This method starts the class.
+	 * 
+	 * @param void
+	 * @return void
+	 */
+	public static void init() {
+		
+		// TODO check what is the purpose of this method.
+	}
+	
+	/**
+	 * This method should return the key pressed or NONE, if there is´
+	 * any key pressed.
+	 * 
+	 * @return
+	 */
+	public static char getKey() {
+		
+		// TODO check what is the purpose of this method.
+		return '-';
+	}
+	
+	/**
+	 * This method returns when the key is pressed of NONE, after 
+	 * 'timeout' milliseconds occurs.
+	 * 
+	 * @return
+	 */
+	public static char waitKey() {
+		
+		// TODO check what is the purpose of this method.
+		return '-';
+	}
+}
